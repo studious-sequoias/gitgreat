36b168d84129828fb65b3d318cddcb935d6dd134
Zachary Weidenbach <zweidenbach@gmail.com>
2016-12-18 19:30:54 -0800

    Removed unnecessary code in the unmount of chatroom.jsx

--- client/app/components/Chatroom.jsx
+++ client/app/components/Chatroom.jsx
@@ -31,10 +31,6 @@ class Chatroom extends React.Component {
 
   componentWillUnmount() {
     socket.removeListener('msgres:all', this.callback);
-    this.setState.removeListener({
-      userId: sessionStorage.getItem('userId'),
-      eventId: sessionStorage.getItem('eventId') || 1 //INSERT EVENT ID HERE
-    });
   }
 
   getMessages() {
